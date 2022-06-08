f144f707f2205239097d79203bbdcc403f95f788
moisesfalcao <moisesfalcao@gmail.com>
2022-06-08 19:19:37 -0300

    Variaveis e IF

--- js/curso.js
+++ js/curso.js
@@ -6,35 +6,40 @@ let quantidade;
 let estoque;
 let saldo;
 let mensagem;
-
+let confirmacao;
 
 cadastroNome = window.prompt("Qual é o seu nome?");
 cadastroUsuario = window.prompt("Qual é o seu usuário?");
-
+confirmacao = window.confirm("Deseja compartilhar os seus dados?");
 
 estoque = 100;
 quantidade = 10;
 
 saldo = estoque - quantidade;
-console.log(saldo);
 saldo = saldo -1;
-console.log(saldo);
 saldo -= 1;
-console.log(saldo);
 
-/* alert(saldo); */
 
-/* 
-cadastroNome = "Moisés";
-cadastroUsuario = "moises";
- */
 cadastroTextoCompleto = "Nome do usuário: ";
 cadastroTextoCompleto += cadastroNome;
 cadastroTextoCompleto += ", login do usuário: ";
 cadastroTextoCompleto += cadastroUsuario;
-console.log(cadastroTextoCompleto);
 
-/* alert(cadastroTextoCompleto); */
+if( confirmacao == true ) {
+    if( cadastroUsuario == "moises" ){
+        console.log("Você precisa pagar sua assinatura");
+    }else{
+        console.log("seja bem vindo(a) " + cadastroUsuario );
+    }
+}else{
+    console.log("O usuário optou por não compartilhar suas informações");
+}
+
+
+
+
+
+
 
 
 cadastroNome = "José";
