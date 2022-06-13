f82400b06c77e6efde71df759c582cba60eba8ef
moisesfalcao <moisesfalcao@gmail.com>
2022-06-13 09:50:11 -0300

    lista dinamica

--- js/aula-03.js
+++ js/aula-03.js
@@ -1,54 +1,31 @@
+
 var alunos = [ 'Boris' , 'Maria', 'Maria de Fátima','Rebeka','Fulano','Beltrano'];
-var objetosAlunos = document.querySelectorAll('main ul li');
-
-
-/*
-window.setTimeout( 
-    function(){  
-        carregaAlunos();
-    }, 5000 );
-    */
-
-window.setInterval(
-    function(){
-        carregaAlunos()
-    },3000);
+var listaDeAlunos = document.querySelector('main ul');
 
 
 function carregaAlunos(){
+    alunos.forEach( function(item , index , array) {
 
-    //window.alert("nova rodada");
+        novaLi = document.createElement('li');
+        novaLi.append(item);
+        listaDeAlunos.append(novaLi);
 
-    objetosAlunos.forEach(function(item,index,array){
-
-        if(index == 0 ){
-    
-            objetosAlunos[index].innerHTML =  "campeão: " + alunos[index];
-            objetosAlunos[index].style.background = "green";
-            objetosAlunos[index].style.fontSize = "40px";
-            objetosAlunos[index].style.fontWeight = "700";
-        
-        }else{
-    
-            if(index % 2 == 0){
-                objetosAlunos[index].innerHTML = index+1 + " lugar:  " + alunos[index];
-                
-            }else{
-                objetosAlunos[index].innerHTML = index+1 + " lugar:  " + alunos[index];
-                objetosAlunos[index].style.background = "#ccc";
-            }
-            
-        }
-    
-        
     });
 }
 
 
-
-
-
-
-
-
-   
+/*
+        if (index === 0 ) {
+            objetosAlunos[index].innerHTML =  "campeão: " + alunos[index];
+            objetosAlunos[index].style.background = "green";
+            objetosAlunos[index].style.fontSize = "40px";
+            objetosAlunos[index].style.fontWeight = "700";
+        } else {
+            if (index % 2 === 0) {
+                objetosAlunos[index].innerHTML = index+1 + " lugar:  " + alunos[index];
+            } else {
+                objetosAlunos[index].innerHTML = index+1 + " lugar:  " + alunos[index];
+                objetosAlunos[index].style.background = "#ccc";
+            }
+        }
+*/
---
+++ js/comentarios.js
@@ -0,0 +1,51 @@
+var comentarios = [];
+comentarios[0] = "Não concordo - Fulano de tal";
+comentarios[1] = "Espero que as ligações de São Paulo Acabem - Sicrano";
+comentarios[2] = "Isso tudo é politicagem - Daniel Silva";
+comentarios[3] = "Vem em boa hora - Marcela Albertin";
+comentarios[4] = "Trabalho em uma empresa que foi multada - Abelardo Tite";
+
+
+
+function mostrarComentarios(){
+
+    var ulComentarios = document.querySelector('article section ul');
+    comentarios.forEach(function( item ,index,array){
+        var novaLi = document.createElement('li');
+        novaLi.append(item);
+        ulComentarios.append(novaLi);
+    });
+
+
+    document.querySelector('article section').style.display = 'block';
+    document.querySelector('article button').style.display = 'none';
+}
+
+
+
+window.onload = function(){
+    document.querySelector('article button').onclick = function(){
+        mostrarComentarios();
+    }
+}
+
+
+
+
+
+
+/*
+
+var comentarios = [];
+comentarios[0] = "Não vejo a hora, não aguento mais tantas ligações - Roberto Souza";
+comentarios[1] = "Será que realmente vão aplicar as multas??? - Marília Pedrosa";
+comentarios[2] = "Toda hora recebendo propaganda no e-mail de empresas - Marinho Costa";
+comentarios[3] = "Acredito que a situação vai melhorar - Carla Meneguel";
+
+    var containerLista = document.querySelector('article section ul');
+    comentarios.forEach(function(item,index,array){
+        var novali = document.createElement('li');
+        novali.append(item);
+        containerLista.append(novali);
+    });
+*/
